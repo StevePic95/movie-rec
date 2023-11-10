1410e84ea97ad3111c0847fb62388c9c99c80810
Stephen <piccol24@students.rowan.edu>
2023-11-10 10:54:29 -0500

    fix node setup logic

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,13 @@
 """
-All this file does is run a uvicorn server to host the backend on port 8000.
+All this file does is run a uvicorn server to host the backend on port 9000.
 """
+
 
 import uvicorn
 import pathlib
 
 
 if __name__ == "__main__":
-
-    print(pathlib.Path.cwd())
 
     # first argument is basically saying "look in the 'app' folder, get the 'app.py' file,
     # then use the 'app' object in that file as the backend"
